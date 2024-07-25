a4b55fa2f82fa88782a8da487517e232372a31a0
Vyacheslav Gonchar <165869963+Crollexx@users.noreply.github.com>
2024-07-25 18:40:27 +0300

    Update 20240704123150-categories.js

--- server/db/seeders/20240704123150-categories.js
+++ server/db/seeders/20240704123150-categories.js
@@ -10,10 +10,8 @@ module.exports = {
           title: "Угадай персонажей по картинкам",
           img: 'client/public/Снимок экрана 2024-07-25 175236.png',
         },
-        {
-          title: "Сериалы",
-          img: "https://cs6.pikabu.ru/post_img/big/2015/04/04/5/1428133715_1088484325.jpg",
-        }
+        
+        
       ],
       {}
     );
